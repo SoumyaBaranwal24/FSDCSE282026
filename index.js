c449f9b994e0304c5ea955d0e520240d266b10bd
SoumyaBaranwal24 <soumyabaranwal62@gmail.com>
2026-07-27 15:38:09 +0530

    sqrt,let,var

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Hello, Soumya!");
+/*function sum(a, b) {
+    return Math.sqrt(a) + Math.sqrt(b);
+}
+
+console.log(sum(25, 9));
+*/
+
+let a = 34;
+if(a>20){
+    let a = 45;
+    console.log("a inside a = " + a);
+
+}
+console.log("a outside a = " + a);
+// let access within block(local scope) while var has both (global scope)
